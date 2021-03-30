19277a226ea96583368da849eb2aa0bd28fbf3bb
Pooja033 <66821210+Pooja033@users.noreply.github.com>
2021-03-30 12:54:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,11 +3,7 @@ class Hello{
   
   public static void main(String args[]){
     
-    system.out.println("Hello Github");
-    
-    for(int i=1;i<=10;i++){
-      
-        system.out.println("Print these numbers" +i);
-    
+    system.out.println("Hello Github Brnach");
+   
   }
 }
